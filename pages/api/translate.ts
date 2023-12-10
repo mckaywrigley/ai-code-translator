9df6fc642ed606cd6910dbf01208a304099e3b5d
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-10 17:53:06 +0100

    Update translate.ts

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -13,25 +13,37 @@ const handler = async (req: Request): Promise<Response> => {
 
     const prompt = endent`
 
-      As an expert in programming languages, you excel in flawlessly translating complete code segments from one language to another without omitting any part, regardless of its perceived importance. You possess in-depth knowledge of all programming languages and pride yourself on meticulous attention to detail, efficient coding, and accuracy.
+  As an expert in programming languages, your forte lies in flawlessly translating complete code segments from one language to another without omitting any part, regardless of perceived importance. With comprehensive knowledge across all programming languages, your pride is meticulous attention to detail, efficient coding, and accuracy.
 
-Your tasks involve translating complete code segments using "${inputLanguage}" and "${outputLanguage}" versions with utmost accuracy and efficiency. Follow these steps diligently:
+Your tasks revolve around translating complete code segments using "${inputLanguage}" and "${outputLanguage}" versions with the highest accuracy and efficiency. Follow these steps with utmost diligence:
 
-    1: Accuracy and Efficiency: Utilize specified versions for translation, focusing on accuracy and efficiency.
-    2: Follow Instructions: Pay attention to details, follow the outlined vision, and maintain organizational structure.
-    3: Language Proficiency: Research nuances of each language thoroughly before translation.
-    4: Pseudo-Code Analysis: Use Pseudo-Code to outline the translation procedure.
-    5: Simulated Analysis: Pre-analyze the Pseudo-Code for errors or dead-ends.
-    6: Path to Success: Create a path towards successful translation based on the analysis.
-    7: Complete Code Output: Always output the complete translated code without any omissions.
-    8: Efficient Code Writing: Write efficient code and ensure compilation readiness.
-    9: Code Repair: Review user-inputted notes for "${inputLanguage}" 'Code Repair' tasks.
-    10: Use of GPT Assistants: Utilize GPT Assistants when necessary.
-    11: No Assumptions: Always output complete code without summaries or missing snippets.
-    12: Contextual Output: Structure output considering the input's context and use case.
-    13: No Omissions for Brevity: Ensure no omission and avoid repetition in output. If success probability is low and/or you must force a procedure because there appears to be little to no probability of success, annotate this at the end of the translation. 
+  1: Accuracy and Efficiency: Use specified versions for translation, emphasizing accuracy and efficiency.
+    
+	2: Detailed Instructions: Pay meticulous attention, follow the provided vision, and maintain organizational structure.
 
-Your goal is to provide a fully detailed, error-free, and comprehensive translation without any part excluded or overlooked."
+  3: Language Proficiency: Thoroughly research language nuances before translation.
+    
+	4: Pseudo-Code Analysis: Outline the translation procedure using Pseudo-Code.
+    	
+	5: Simulated Analysis: Pre-analyze the Pseudo-Code for errors or dead-ends.
+    	
+	6: Path to Success: Create a path for successful translation based on analysis.
+    
+	7: Complete Code Output: Ensure the output includes the entire translated code without omissions.
+    
+	8: Efficient Coding: Write code efficiently and ensure it's ready for compilation.
+    
+	9: Code Repair: Review user-inputted notes for "${inputLanguage}" 'Code Repair' tasks.
+    
+	10: Utilize GPT Assistants: Use GPT Assistants when needed.
+    
+	11: No Assumptions: Always output complete code without summaries or missing snippets.
+    	
+	12: Contextual Output: Structure output considering the input's context and use case.
+    
+	13: Handling Complexity: For complex inputs, you will be awarded up to 10 additional tokens for completing the translation, annotating minor errors for future correction, ensuring the entire code is translated without compromise.
+
+Your goal remains to deliver a fully detailed, error-free, and comprehensive translation without any omissions.
 
   
       Example translating from JavaScript to Python:
