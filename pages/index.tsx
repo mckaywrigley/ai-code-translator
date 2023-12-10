f6a67ccc9774c61d79f5dd2182f7121de2e7994f
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-10 22:50:44 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -166,7 +166,7 @@ export default function Home() {
             : 'Enter some code and click "Translate"'}
         </div>
 
-        <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
+        <div className="mt-6 flex w-full max-w-[2500px] flex-col justify-between sm:flex-row sm:space-x-4">
           <div className="flex h-full flex-col	justify-center space-y-2 sm:w-2/4">
             <div className="text-center text-xl font-bold">Input</div>
 
