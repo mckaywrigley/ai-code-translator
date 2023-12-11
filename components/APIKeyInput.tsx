5be67ba15a2157520f50d19993681ee8452e59f1
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-11 12:13:17 +0100

    Revert "Update APIKeyInput.tsx"

--- components/APIKeyInput.tsx
+++ components/APIKeyInput.tsx
@@ -5,13 +5,12 @@ interface Props {
 
 export const APIKeyInput: React.FC<Props> = ({ apiKey, onChange }) => {
   return (
-<form>
-      <input
-        className="mt-1 h-[26px] w-[400px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-        type="password"
-        placeholder="OpenAI API Key"
-        onChange={(e) => onChange(e.target.value)}
-      />
-   </form>
+    <input
+      className="mt-1 h-[24px] w-[380px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      type="password"
+      placeholder="OpenAI API Key"
+      value={apiKey}
+      onChange={(e) => onChange(e.target.value)}
+    />
   );
 };
