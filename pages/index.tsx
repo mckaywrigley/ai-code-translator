0b37606fa520a118c214a77282f15b0270e336eb
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-16 14:35:04 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [outputLanguage, setOutputLanguage] = useState<string>('C#');
   const [inputCode, setInputCode] = useState<string>('');
   const [outputCode, setOutputCode] = useState<string>('');
-  const [model, setModel] = useState<OpenAIModel>('gpt-4-1106-preview');
+  const [model, setModel] = useState<OpenAIModel>('gpt-3.5');
   const [loading, setLoading] = useState<boolean>(false);
   const [hasTranslated, setHasTranslated] = useState<boolean>(false);
   const [apiKey, setApiKey] = useState<string>('');
